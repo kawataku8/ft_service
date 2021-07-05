2a5d2f2394f5eed8867009472e2a6e87a2a79941
kawataku8 <tak.354354@gmail.com>
2021-07-05 16:35:44 +0900

    DONE I guess

--- ftps/srcs/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-/telegraf-1.17.2/usr/bin/telegraf --config /etc/telegraf/telegraf.conf &
-
-/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
--- setup.sh
+++ setup.sh
@@ -5,23 +5,24 @@ sudo minikube start --vm-driver=none --extra-config=apiserver.service-node-port-
 sudo kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 sudo kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 sudo kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-sudo kubectl apply -f metalLB.yaml
+sudo kubectl apply -f ./srcs/metallb/metalLB.yaml
 
 # docker image build
-docker build -t tk_nginx_img ./nginx
-docker build -t tk_grafana_img ./Grafana
-docker build -t tk_influx_img ./influxDB
-docker build -t tk_phpadmin_img ./phpmyadmin
-docker build -t tk_wp_img ./wordpress
-docker build -t tk_mysql_img ./mysql
-docker build -t tk_ftps_img ./ftps
+docker build -t tk_nginx_img ./srcs/nginx
+docker build -t tk_grafana_img ./srcs/Grafana
+docker build -t tk_influx_img ./srcs/influxDB
+docker build -t tk_phpadmin_img ./srcs/phpmyadmin
+docker build -t tk_wp_img ./srcs/wordpress
+docker build -t tk_mysql_img ./srcs/mysql
+docker build -t tk_ftps_img ./srcs/ftps
 
 # apply a configuration to a resource
-sudo kubectl apply -f ./nginx/nginx.yaml
-sudo kubectl apply -f ./ftps/ftps_deploy.yaml
-sudo kubectl apply -f ./mysql/mysql.yaml
-sudo kubectl apply -f ./wordpress/wp.yaml
-sudo kubectl apply -f ./phpmyadmin/phpadmin.yaml
-sudo kubectl apply -f ./influxDB/influx.yaml
-sudo kubectl apply -f ./Grafana/grafana.yaml
-# sudo kubectl apply -f ./debug_pod/debugpod.yaml
+sudo kubectl apply -f ./srcs/nginx/nginx.yaml
+sudo kubectl apply -f ./srcs/ftps/ftps_deploy.yaml
+sudo kubectl apply -f ./srcs/mysql/mysql.yaml
+sudo kubectl apply -f ./srcs/phpmyadmin/phpadmin.yaml
+sudo kubectl apply -f ./srcs/influxDB/influx.yaml
+sudo kubectl apply -f ./srcs/Grafana/grafana.yaml
+sudo kubectl apply -f ./srcs/wordpress/wp.yaml
+
+# sudo minikube dashboard
